b62591719091d3079f54e56a4056e511ca87b282
Dongwoo Kim <dongwookim@Dongwooui-MacBookPro.local>
2022-03-18 18:06:58 -0700

    fix(Board.js-Square.js): implemented tictactoe and deleted square js

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,15 +1,74 @@
-import React from "react";
+import React, { useState } from "react";
 import './styles/Board.css';
-import Square from "./Square";
+import './styles/Square.css';
+
+function calculateWinner(squares){
+  const winning_line=[
+      [0,1,2],
+      [3,4,5],
+      [6,7,8],
+      [0,3,6],
+      [1,4,7],
+      [2,5,8],
+      [0,4,8],
+      [2,4,6]
+  ]
+
+  for(let i=0;i<winning_line.length;i++){
+      const[idx1, idx2, idx3]=winning_line[i];
+      if(squares[idx1]&&squares[idx1]===squares[idx2] && squares[idx1]===squares[idx3]){
+          return squares[idx1];
+      }
+
+  }
+  return null;
+}
+
+function calculateNextValue(squares){
+      return squares.filter(Boolean).length%2===0?"X":"O"
+}
+
+function calculateStatus(winner, squares, nextValue){
+  if(winner){
+    const winnerState=`Winner : ${winner}`;
+    squares.every(Boolean);
+    return winnerState;
+  }else{
+    return `Next player: ${nextValue}`;
+  }
+}
+
+
+
 
 function Board() {
+    const [board, setBoard]=useState(Array(9).fill(null));
+    
+    const winner=calculateWinner(board);
+    const nextValue=calculateNextValue(board);
+    const status=calculateStatus(winner, board, nextValue)
 
-    const status = 'Next player: X';
-
-    function renderSquare(i) {
-        return <Square />;
+    function selectSquare(square){
+      if(winner|| board[square]){
+        return
+      }
+      
+      const squaresCopy=[...board]
+      squaresCopy[square]=nextValue
+      setBoard(squaresCopy)
     }
 
+    function restart(){
+      setBoard(Array(9).fill(null))
+    }
+    
+    function renderSquare(i) {
+      return (
+      <button className="square" onClick={()=>selectSquare(i)}>
+        {board[i]}
+      </button>)
+    }
+    
     return (  
         <div>
           <div className="board-row">
@@ -28,6 +87,7 @@ function Board() {
             {renderSquare(8)}
           </div>
           <div className="status">{status}</div>
+          <button onClick={restart}>Restart!</button>
         </div>
     )
 }
